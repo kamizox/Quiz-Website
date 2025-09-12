9d761829ce3ff9402ce55b61dcfd492afe0c4de4
Kamran Alam <115376744+kamizox@users.noreply.github.com>
2025-09-12 21:36:37 +0500

    initial

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+const quiz = [
+    {
+        question: "what is your name?",
+        options: ["Ali", "Danish", "Uzair","Amir"],
+        correctAnswer: "Ali"
+    }
+]
+
+
+
+var questionElement = document.getElementById("Question")
+questionElement.innerHTML = 
