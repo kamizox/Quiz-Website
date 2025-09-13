58b64a17fb9a230287a8b3b94f31f7b188a80415
Kamran Alam <115376744+kamizox@users.noreply.github.com>
2025-09-14 02:55:12 +0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var score = 0;
+var stopNum = 0;
+var clicked = false;
 var scoreRec = document.getElementById("scoreRec");
 
 const quiz = [
@@ -59,11 +61,10 @@ var currentQuestion = 0;
 function rederQestion() {
     var questionElement = document.getElementById("Question")
     questionElement.innerHTML = quiz[currentQuestion].question;
-    
+
     var OptionListElement = document.getElementById("QuestionOptions");
     OptionListElement.innerHTML = '';
-    for(var i=0; i<quiz[currentQuestion].options.length; i++)
-    {
+    for (var i = 0; i < quiz[currentQuestion].options.length; i++) {
         OptionListElement.innerHTML += `<li onclick="checkCorrect(event)"> 
         <h3 style=" width:520px; height:40px;
          border-radius:15px;
@@ -75,22 +76,32 @@ function rederQestion() {
 
 rederQestion();
 
-function nextQusetion(){
-    currentQuestion++;
-    rederQestion();
+function nextQusetion() {
+    
+    if (clicked == true) {
+        currentQuestion++;
+        rederQestion();
+        stopNum++;
+        console.log(currentQuestion)
+        clicked = false;
+    }else{
+        alert("Please Chose any option")
+    }
+
 }
 
-function checkCorrect(event)
-{
-    if(quiz[currentQuestion].correctAnswer.trim() === event.target.innerText.trim())
-    {
+
+function checkCorrect(event) {
+    if (quiz[currentQuestion].correctAnswer.trim() === event.target.innerText.trim()) {
         event.target.style.backgroundColor = "green";
         event.target.style.color = "White";
-        score +=10;
-        scoreRec.innerHTML = "Score"+score;
-    }else{
+        score += 10;
+        scoreRec.innerHTML = "Score" + score;
+        clicked = true;
+    } else {
         event.target.style.backgroundColor = "red";
         event.target.style.color = "White";
+        clicked = true;
     }
 
 }
